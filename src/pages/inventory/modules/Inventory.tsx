13bcf20e38c8fde3cdcce32de82e69678d426db9
Karina <karina@mxn.kz>
2024-06-05 10:12:43 +0500

    changed design in inventory page

--- src/pages/inventory/modules/Inventory.tsx
+++ src/pages/inventory/modules/Inventory.tsx
@@ -1,20 +1,14 @@
 import { Box, Button, Select, useDisclosure } from '@chakra-ui/react'
 import InventoryTable from '../components/InventoryTable'
 import CorrectModal from '../components/Modal'
-import UniversalComponent from '@/components/ui/UniversalComponent'
 import { useURLParameters } from '@/utils/hooks/useURLParameters'
 import DateRange from '@/components/DateRange'
 import { useApi } from '@/utils/services/axios'
 import { ProviderGoodsType } from '@/utils/types/providerGoog.types'
 
-interface Place {
-    label: string
-}
-
 const Inventory = () => {
     const { setParam, getParam } = useURLParameters()
     const { data: providerGoodsData } = useApi<ProviderGoodsType[]>('providerGoods')
-    const { data: placesData } = useApi<Place[]>('place')
     const { isOpen, onOpen, onClose } = useDisclosure()
 
     const onSuccess = () => {
@@ -23,59 +17,44 @@ const Inventory = () => {
 
     return (
         <>
-            <UniversalComponent>
-                <Box width={'100%'} height={'94%'} p={5} mt={2}>
-                    <Box
-                        marginBottom={8}
-                        display={'flex'}
-                        justifyContent={'space-between'}
-                        height={'5%'}
-                    >
-                        <Box display={'flex'} gap={'15px'} width={'fit-content'} w={'100%'}>
-                            <DateRange />
-                            <Select
-                                placeholder="Товар"
-                                w={'20%'}
-                                size={'sm'}
-                                borderRadius={5}
-                                defaultValue={getParam('name')}
-                                onChange={(e) => {
-                                    setParam('name', e.target.value)
-                                }}
-                            >
-                                {providerGoodsData?.map((item, index) => (
-                                    <option key={index} value={item.goods}>
-                                        {item.goods}
-                                    </option>
-                                ))}
-                            </Select>
-                            <Select
-                                placeholder="Место"
-                                w={'20%'}
-                                size={'sm'}
-                                borderRadius={5}
-                                defaultValue={getParam('place')}
-                                onChange={(e) => {
-                                    setParam('place', e.target.value)
-                                }}
-                            >
-                                {placesData?.map((item, index) => (
-                                    <option key={index} value={item.label}>
-                                        {item.label}
-                                    </option>
-                                ))}
-                            </Select>
-                        </Box>
+            <Box
+                display="flex"
+                flexDirection="column"
+                maxHeight={'calc(95% - 2.5rem)'}
+                height="100vh"
+                p={5}
+                mt={2}
+            >
+                <Box marginBottom={6} display={'flex'} justifyContent={'space-between'}>
+                    <Box width={'100%'} display={'flex'} gap={'15px'}>
+                        <DateRange />
+                        <Select
+                            placeholder="Товар"
+                            w={'20%'}
+                            size={'sm'}
+                            borderRadius={5}
+                            defaultValue={getParam('name')}
+                            onChange={(e) => {
+                                setParam('name', e.target.value)
+                            }}
+                        >
+                            {providerGoodsData?.map((item, index) => (
+                                <option key={index} value={item.goods}>
+                                    {item.goods}
+                                </option>
+                            ))}
+                        </Select>
+                    </Box>
 
-                        <Button colorScheme="purple" onClick={onOpen}>
-                            Корректировка
-                        </Button>
-                    </Box>
-                    <Box height={'calc(95% - 2.5rem)'} position={'relative'}>
-                        <InventoryTable />
-                    </Box>
+                    <Button colorScheme="purple" onClick={onOpen}>
+                        Корректировка
+                    </Button>
                 </Box>
-            </UniversalComponent>
+                <Box position={'relative'}>
+                    <InventoryTable />
+                </Box>
+            </Box>
+
             <CorrectModal isOpen={isOpen} onClose={onClose} onSuccess={onSuccess} />
         </>
     )
