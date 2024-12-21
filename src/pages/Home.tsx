512cc66c24603cccc8d99063addbff1b44867424
Edelmir Muratkanov <muratkanov02@gmail.com>
2024-12-21 23:19:57 +0500

    refactor: :recycle: display mobile nav bar if user uses mobile and he is client
    #78

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,16 @@ import MobileNavbar from '@/components/MobileNavbar'
 import IsMobile from '@/utils/helpers/isMobile'
 import mainBg from '@/assets/main-bg.png'
 import { AspectRatio, Image } from '@chakra-ui/react'
+import getUserInfo from '@/utils/helpers/getUserInfo'
 
 const Home = () => {
+    const user = getUserInfo()
+    const classes = JSON.parse(user?.class || '[]') as { label: string }[]
+    const isClient = classes.map(({ label }) => label === 'Реализатор')
+
     return (
         <>
-            {IsMobile() ? <MobileNavbar /> : <TopNavBar />}
+            {IsMobile() && isClient ? <MobileNavbar /> : <TopNavBar />}
             <AspectRatio height='90dvh' ratio={1920 / 1080}>
                 <Image src={mainBg} alt='bg' objectFit='cover' />
             </AspectRatio>
