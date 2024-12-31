6cdabcb3596d997a8d94af0925a2b9577440fcd1
Edelmir Muratkanov <muratkanov02@gmail.com>
2024-12-31 11:33:47 +0500

    feat: :sparkles: add debt property, calculate total & display
    #88

--- src/pages/reports/modules/ReconciliationView.tsx
+++ src/pages/reports/modules/ReconciliationView.tsx
@@ -1,19 +1,20 @@
 import DateRange from '@/components/DateRange'
-import { TableContainer, Thead } from '@/components/ui'
+import { TableContainer, Tfoot, Thead } from '@/components/ui'
 import { useURLParameters } from '@/utils/hooks/useURLParameters'
 import { useApi } from '@/utils/services/axios'
-import { Box, Button, Select, Table, Tbody, Td, Th, Tr } from '@chakra-ui/react'
+import { Box, Select, Table, Tbody, Td, Th, Tr } from '@chakra-ui/react'
 import dayjs from 'dayjs'
-import { useEffect } from 'react'
+import { useEffect, useMemo } from 'react'
 
 type ReportData = {
     fullReports: {
         adjustedDate: string
         sales: number
         returns: number
-        overhead: string
-        payments: string
+        overhead: number
+        payments: number
         magazines: { [x: string]: number }[]
+        debt: number
     }[]
     allMagazines: string[]
 }
@@ -29,14 +30,27 @@ export default function ReconciliationView() {
 
     const { data: contragents } = useApi<Contragent[]>('contragent')
     const { data } = useApi<ReportData>(`reports/reconciliation?${getURLs().toString()}`)
+    const total = useMemo(
+        () =>
+            data?.fullReports.reduce(
+                (acc, cur) => {
+                    acc.sales += cur.sales
+                    acc.returns += cur.returns
+                    acc.payments += cur.payments
+                    acc.debt += cur.debt
+
+                    return acc
+                },
+                { sales: 0, returns: 0, payments: 0, debt: 0 },
+            ),
+        [data],
+    )
 
     useEffect(() => {
         if (!getParam('contragentId')) {
             setParam('contragentId', contragents?.[0].id.toString()!)
         }
     })
-
-    const exportExcel = () => {}
 
     return (
         <Box>
@@ -66,14 +80,6 @@ export default function ReconciliationView() {
                                 ))}
                         </Select>
                     </Box>
-                    <Box display='flex' gap='15px'>
-                        <Button type='button' onClick={exportExcel}>
-                            Экспорт в Excel
-                        </Button>
-                        <Button type='button' onClick={() => window.print()}>
-                            Экспорт в PDF
-                        </Button>
-                    </Box>
                 </Box>
                 <Box>
                     <TableContainer style={{ width: '100%', height: '100%', overflowY: 'auto' }}>
@@ -87,6 +93,7 @@ export default function ReconciliationView() {
                                     <Th>Выплата</Th>
                                     <Th>Сверху</Th>
                                     {data?.allMagazines.map((item) => <Th key={item}>{item}</Th>)}
+                                    <Th>Долг</Th>
                                 </Tr>
                             </Thead>
                             <Tbody>
@@ -97,12 +104,13 @@ export default function ReconciliationView() {
                                             <Td>{dayjs(item.adjustedDate).format('DD.MM.YYYY')}</Td>
                                             <Td>{(item.sales || 0).formatted()}</Td>
                                             <Td>{(item.returns || 0).formatted()}</Td>
-                                            <Td>{(+item.payments || 0).formatted()}</Td>
-                                            <Td>{(+item.overhead || 0).formatted()}</Td>
+                                            <Td>{(item.payments || 0).formatted()}</Td>
+                                            <Td>{(item.overhead || 0).formatted()}</Td>
                                             {data.allMagazines.map((am) => (
                                                 // @ts-ignore
                                                 <Td key={am}>{item.magazines[am]}</Td>
                                             ))}
+                                            <Td>{(item.debt || 0).formatted()}</Td>
                                         </Tr>
                                     ))
                                 ) : (
@@ -111,6 +119,28 @@ export default function ReconciliationView() {
                                     </Tr>
                                 )}
                             </Tbody>
+                            <Tfoot>
+                                <Tr>
+                                    <Th fontSize={15} color='#000'>
+                                        ИТОГО
+                                    </Th>
+                                    <Th />
+                                    <Th fontSize={15} color='#000'>
+                                        {total?.sales}
+                                    </Th>
+                                    <Th fontSize={15} color='#000'>
+                                        {total?.returns}
+                                    </Th>
+                                    <Th fontSize={15} color='#000'>
+                                        {total?.payments}
+                                    </Th>
+                                    <Th />
+                                    {data?.allMagazines.map(() => <Th />)}
+                                    <Th fontSize={15} color='#000'>
+                                        {total?.debt}
+                                    </Th>
+                                </Tr>
+                            </Tfoot>
                         </Table>
                     </TableContainer>
                 </Box>
